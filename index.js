b242fce9ea44941bbd5b3bd106ac65e316471bad
Isabel Palacios <issabelpalacios@gmail.com>
2023-02-07 20:16:07 -0300

    ADD CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 const heroeRouter = require("./routes/HeroeRoutes");
 require('dotenv').config();
 
@@ -8,7 +9,10 @@ const app = express();
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+//routes
 app.use("/api/heroes", heroeRouter);
+//conexion a la base de datos
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/CRUD",
     {
@@ -19,10 +23,11 @@ mongoose.connect(
       if (err) {
         console.log(err);
       } else {
-        console.log("Connected to MongoDB");
+        console.log("Base de datos conectada");
       }
     }
   );
+//inicializar el servidor
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3001");
   console.log(process.env.PORT)
